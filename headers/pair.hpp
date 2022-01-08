5078be2780758199eb13b536cb69152db20546b4
João Ceia <joao.p.ceia@gmail.com>
2022-01-08 16:01:33 +0000

    updated pair.hpp

--- headers/pair.hpp
+++ headers/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 02:42:16 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/20 02:51:37 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/01/08 16:00:18 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,92 @@
 namespace ft
 {
     template <class T1, class T2>
-    class pair
+    struct pair
     {
-        public:
-            pair(T1 t1, T t2);
-            pair(const pair &rhs);
-            ~pair(void);
-            
-            pair &operator=(const pair &rhs);
+    public:
+        // Member types
+        typedef T1  first_type;
+        typedef T2  second_type;
 
-            T1 first;
-            T2 second;
+        // ---------------------------------------------------------------------
+        //                              Constructors
+        // ---------------------------------------------------------------------
+
+        // Default constructor
+        pair()
+            : first(), second()
+        {}
+
+        // Copy constructor
+        pair(const pair& p)
+            : first(p.first), second(p.second)
+        {}
+
+        // Initialization constructor
+        pair(const first_type& x, const second_type& y)
+            : first(x), second(y)
+        {}
+
+        // ---------------------------------------------------------------------
+        // ~pair() {}
+        
+        // Assignment operator overload
+        pair &operator=(const pair &rhs)
+        {
+            first = rhs.first;
+            second = rhs.second;
+            return *this;
+        }
+
+        // ---------------------------------------------------------------------
+        //                          Member variables
+        // ---------------------------------------------------------------------
+
+        T1 first;
+        T2 second;
     };
+
+    // ---------------------------------------------------------------------
+    //                          Relational operators
+    // ---------------------------------------------------------------------
+
+
+    template <class T1, class T2>
+    bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+    {
+        return lhs.first == rhs.first && lhs.second == rhs.second;
+    }
+
+    template <class T1, class T2>
+    bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    template <class T1, class T2>
+    bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+    {
+        return lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+    }
+
+    template <class T1, class T2>
+    bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+    {
+        return !(rhs < lhs);
+    }
+
+    template <class T1, class T2>
+    bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+    {
+        return rhs < lhs;
+    }
+
+    template <class T1, class T2>
+    bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+    {
+        return !(lhs < rhs);
+    }
 } // namespace ft
+
 
 #endif
