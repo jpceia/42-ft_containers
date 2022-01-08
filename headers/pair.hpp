d240104dfde5d1f8321815553a9f54542c35179e
João Ceia <joao.p.ceia@gmail.com>
2022-01-08 16:04:55 +0000

    addded make_pair function

--- headers/pair.hpp
+++ headers/pair.hpp
@@ -101,6 +101,17 @@ namespace ft
     {
         return !(lhs < rhs);
     }
+
+    // ---------------------------------------------------------------------
+    //                          Make_pair function
+    // ---------------------------------------------------------------------
+
+    template<typename T1, typename T2>
+    pair<T1, T2> make_pair(T1 x, T2 y)
+    {
+        return pair<T1, T2>(x, y);
+    }
+
 } // namespace ft
 
 
