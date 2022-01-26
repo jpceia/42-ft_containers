9f6f959bec2e71aae565fb7c230c3f15d5f76f8b
João Ceia <joao.p.ceia@gmail.com>
2022-01-26 14:25:42 +0000

    added spacing in tester

--- tests/test_vector_reserve.cpp
+++ tests/test_vector_reserve.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 15:41:04 by jpceia            #+#    #+#             */
-/*   Updated: 2022/01/11 21:31:08 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/01/26 14:25:24 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@ int main()
     ft::vector<int> foo;
     sz = foo.capacity();
     std::cout << "making foo grow:\n";
-    for (int i=0; i<100; ++i) {
+    for (int i = 0; i < 100; ++i)
+    {
         foo.push_back(i);
-        if (sz!=foo.capacity()) {
-        sz = foo.capacity();
-        std::cout << "capacity changed: " << sz << '\n';
+        if (sz != foo.capacity())
+        {
+            sz = foo.capacity();
+            std::cout << "capacity changed: " << sz << '\n';
         }
     }
 
@@ -38,11 +40,13 @@ int main()
     sz = bar.capacity();
     bar.reserve(100);   // this is the only difference with foo above
     std::cout << "making bar grow:\n";
-    for (int i=0; i<100; ++i) {
+    for (int i = 0; i < 100; ++i)
+    {
         bar.push_back(i);
-        if (sz!=bar.capacity()) {
-        sz = bar.capacity();
-        std::cout << "capacity changed: " << sz << '\n';
+        if (sz != bar.capacity())
+        {
+            sz = bar.capacity();
+            std::cout << "capacity changed: " << sz << '\n';
         }
     }
 
