4fc0a0ae86c504e5e8094e3480d6c105a1b611b5
João Ceia <joao.p.ceia@gmail.com>
2022-01-26 10:46:05 +0000

    minor fixes in tests

--- tests/test_lexicographical_compare.cpp
+++ tests/test_lexicographical_compare.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 21:29:40 by jpceia            #+#    #+#             */
-/*   Updated: 2022/01/11 21:31:33 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/01/26 10:35:03 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
     #include <algorithm>    // std::lexicographical_compare
     namespace ft = std;
 #else
-    # include "lexicographical_compare.hpp"
+    # include "iterator/lexicographical_compare.hpp"
 #endif
 
 // a case-insensitive comparison function:
