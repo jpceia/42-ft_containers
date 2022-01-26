cb5d7fda0527fa35df26e77dd79fc2773957bf2f
João Ceia <joao.p.ceia@gmail.com>
2022-01-26 10:48:20 +0000

    using own reverse iterator

--- headers/vector.hpp
+++ headers/vector.hpp
@@ -17,6 +17,10 @@
 # include <stdexcept>
 # include <iostream>
 
+# include "iterator/lexicographical_compare.hpp"
+# include "iterator/iterator_traits.hpp"
+# include "iterator/reverse_iterator.hpp"
+
 /*
  * Member types
  * ---------------------------------------------------------------------------
@@ -48,22 +52,25 @@
 
 namespace ft
 {
-    template <class T, class Alloc = ::std::allocator<T> >
+    template <typename T, typename Alloc = ::std::allocator<T> >
     class vector
     {
     public:
-        typedef T                                                   value_type;
-        typedef Alloc                                               allocator_type;
-        typedef typename allocator_type::reference                  reference;
-        typedef typename allocator_type::const_reference            const_reference;
-        typedef typename allocator_type::pointer                    pointer;
-        typedef typename allocator_type::const_pointer              const_pointer;
-        typedef typename allocator_type::pointer                    iterator;
-        typedef typename allocator_type::const_pointer              const_iterator;
-        typedef typename ::std::reverse_iterator<iterator>          reverse_iterator;
-        typedef typename ::std::reverse_iterator<const_iterator>    const_reverse_iterator;
-        typedef typename allocator_type::difference_type            difference_type;
-        typedef typename allocator_type::size_type                  size_type;
+        // ---------------------------------------------------------------------
+        // Member types
+        // ---------------------------------------------------------------------
+        typedef T                                           value_type;
+        typedef Alloc                                       allocator_type;
+        typedef typename allocator_type::reference          reference;
+        typedef typename allocator_type::const_reference    const_reference;
+        typedef typename allocator_type::pointer            pointer;
+        typedef typename allocator_type::const_pointer      const_pointer;
+        typedef typename allocator_type::pointer            iterator;
+        typedef typename allocator_type::const_pointer      const_iterator;
+        typedef ft::reverse_iterator<iterator>              reverse_iterator;
+        typedef ft::reverse_iterator<const_iterator>        const_reverse_iterator;
+        typedef typename allocator_type::difference_type    difference_type;  // iterator_traits<iterator>::difference_type
+        typedef typename allocator_type::size_type          size_type;      // iterator_traits<iterator>::size_type
 
         // ---------------------------------------------------------------------
         // Constructors
