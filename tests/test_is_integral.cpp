255ce30b7c6ac0fee9d85c26ace23848701d302f
João Ceia <joao.p.ceia@gmail.com>
2022-01-27 07:29:45 +0000

    added test for is_integral

---
+++ tests/test_is_integral.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_is_integral.cpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 21:16:57 by jpceia            #+#    #+#             */
+/*   Updated: 2022/01/26 21:23:44 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>     // std::cout, std::boolalpha
+
+#if USE_STL
+    #include <type_traits>    // std::lexicographical_compare
+    namespace ft = std;
+#else
+    # include "type_traits/is_integral.hpp"
+#endif
+
+int main() {
+    std::cout << std::boolalpha;
+    std::cout << "is_integral:" << std::endl;
+    std::cout << "char: " << ft::is_integral<char>::value << std::endl;
+    std::cout << "int: " << ft::is_integral<int>::value << std::endl;
+    std::cout << "float: " << ft::is_integral<float>::value << std::endl;
+    return 0;
+}
