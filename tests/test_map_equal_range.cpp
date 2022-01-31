3f300488c227e7883a72e997fe98658517640352
João Ceia <joao.p.ceia@gmail.com>
2022-01-31 20:54:25 +0000

    added equal range test

--- tests/test_map_equal_range.cpp
+++ tests/test_map_equal_range.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 20:48:03 by jpceia            #+#    #+#             */
-/*   Updated: 2022/01/30 14:53:24 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/01/31 13:56:19 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,25 @@
 #else
     # include "map.hpp"
 #endif
+
+// map::equal_range
+
+int main ()
+{
+    ft::map<char,int> mymap;
+
+    mymap['a']=10;
+    mymap['b']=20;
+    mymap['c']=30;
+
+    ft::pair<ft::map<char,int>::iterator, ft::map<char,int>::iterator>  ret;
+    ret = mymap.equal_range('b');
+
+    std::cout << "lower bound points to: ";
+    std::cout << ret.first->first << " => " << ret.first->second << '\n';
+
+    std::cout << "upper bound points to: ";
+    std::cout << ret.second->first << " => " << ret.second->second << '\n';
+
+    return 0;
+}
