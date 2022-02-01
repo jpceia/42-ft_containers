52c92a359de5fc496e62e4bedba41e6e374e3e56
João Ceia <joao.p.ceia@gmail.com>
2022-02-01 10:43:26 +0000

    unified rbegin and rend

--- tests/test_vector_rbegin_rend.cpp
+++ tests/test_vector_rbegin_rend.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_vector_rend.cpp                               :+:      :+:    :+:   */
+/*   test_vector_rbegin_rend.cpp                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/11 15:31:23 by jpceia            #+#    #+#             */
-/*   Updated: 2022/01/11 21:31:08 by jpceia           ###   ########.fr       */
+/*   Created: 2022/01/11 15:30:37 by jpceia            #+#    #+#             */
+/*   Updated: 2022/02/01 10:43:11 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ int main()
 {
     ft::vector<int> myvector (5);  // 5 default-constructed ints
 
-    ft::vector<int>::reverse_iterator rit = myvector.rbegin();
-
     int i=0;
-    for (rit = myvector.rbegin(); rit!= myvector.rend(); ++rit)
-    *rit = ++i;
+
+    ft::vector<int>::reverse_iterator rit = myvector.rbegin();
+    for (; rit!= myvector.rend(); ++rit)
+        *rit = ++i;
 
     std::cout << "myvector contains:";
     for (ft::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
-    std::cout << ' ' << *it;
+        std::cout << ' ' << *it;
     std::cout << '\n';
 
     return 0;
