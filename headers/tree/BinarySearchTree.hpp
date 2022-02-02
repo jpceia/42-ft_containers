1ab077aab0fee1bf5786980d1b082d3cffea3119
Joao Ceia <jceia@student.42lisboa.com>
2022-02-02 16:07:00 +0000

    checking if parent is not NULL before changing its value

--- headers/tree/BinarySearchTree.hpp
+++ headers/tree/BinarySearchTree.hpp
@@ -149,27 +149,36 @@ namespace ft
         {
             if (!node->left && !node->right) // case 1: leaf node
             {
+                if (node->parent)
+                {
                 if (node->parent->left == node) // node is left child
                     node->parent->left = NULL;
                 else                            // node is right child
                     node->parent->right = NULL;
+                }
                 _free(node);
             }
             else if (node->right) // case 2: right child only
             {
+                if (node->parent)
+                {
                 if (node->parent->left == node) // node is left child
                     node->parent->left = node->right;
                 else                            // node is right child
                     node->parent->right = node->right;
+                }
                 node->right->parent = node->parent;
                 _free(node);
             }
             else if (node->left) // case 3: left child only
             {
+                if (node->parent)
+                {
                 if (node->parent->left == node)
                     node->parent->left = node->left;
                 else
                     node->parent->right = node->left;
+                }
                 node->left->parent = node->parent;
                 _free(node);
             }
@@ -180,10 +189,13 @@ namespace ft
                 new_node->left = node->left;
                 new_node->right = node->right;
                 new_node->parent = node->parent;
+                if (node->parent)
+                {
                 if (node->parent->left == node)
                     node->parent->left = new_node;
                 else
                     node->parent->right = new_node;
+                }
                 _free(node);
                 _erase(successor, successor->data);
             }
