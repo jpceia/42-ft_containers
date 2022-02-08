54f44324d7e71a1db73e039c3c0029767218e6e6
Joao Ceia <jceia@student.42lisboa.com>
2022-02-08 17:36:38 +0000

    changed test for test_vector_end

--- tests/test_vector_end.cpp
+++ tests/test_vector_end.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_vector_end.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 15:30:00 by jpceia            #+#    #+#             */
-/*   Updated: 2022/02/06 00:40:42 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/02/08 17:07:14 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,25 @@
 
 int main()
 {
-    std::cout << "TEST END" << std::endl;
     ft::vector<int> myvector;
     for (int i = 1; i <= 5; i++)
         myvector.push_back(i);
 
     std::cout << "myvector contains:";
-    for (ft::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
-        std::cout << ' ' << *it;
+    ft::vector<int>::iterator it = myvector.end();
+    *(it + 2) = 100;
+    while (it != myvector.begin())
+    {
+        std::cout << ' ' << *(it - 1);
+        --it;
+    }
+    std::cout << '\n';
+    ft::vector<int>::const_iterator cit = myvector.end();
+    while (cit != myvector.begin())
+    {
+        std::cout << ' ' << *(cit - 1);
+        --cit;
+    }
     std::cout << '\n';
 
     return 0;
