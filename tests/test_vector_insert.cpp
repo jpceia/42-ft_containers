b9d81be0f5a2c4e58c92226c4d0beaa145f13d21
João Ceia <joao.p.ceia@gmail.com>
2022-04-05 09:05:43 +0100

    added test for vector::insert

--- tests/test_vector_insert.cpp
+++ tests/test_vector_insert.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 15:49:58 by jpceia            #+#    #+#             */
-/*   Updated: 2022/02/06 00:52:14 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/04/03 05:36:29 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,13 @@ int main()
 
     it = myvector.begin();
     it = myvector.insert(it, 200);
+    it = myvector.insert(it, 300);
+    it = myvector.insert(it, 400);
+    it = myvector.insert(it, 500);
     print_vector(myvector);
 
-    myvector.insert(it, 2, 300);
+    it = myvector.begin() + 3;
+    myvector.insert(it, 2, 123);
     print_vector(myvector);
 
     // "it" no longer valid, get a new one:
